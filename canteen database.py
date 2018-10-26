dd1752ab134b2803ce142e2f5df3e51cbeac72a6
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2018-10-26 22:37:59 +0800

    canteen database

---
+++ canteen database.py
@@ -0,0 +1,145 @@
+# canteen database
+canteen_db={
+  'Food Court 1': {
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'63343033',
+    'Stalls':{
+      'Stall 101':[4,'Halal',5],
+      'Stall 102':[5,'Japanese',6],
+      'Stall 103':[5,'Japanese',6],
+      'Stall 104':[4, 'Vietnamese', 4],
+      'Stall 105':[2, 'Indian Vegetarian', 7],
+      'Stall 106':[2, 'Korean', 5],
+      'Stall 107':[1, 'Japanese', 3],
+      'Stall 108':[3, 'Chinese', 4],
+      'Stall 109':[4, 'Vietnamese', 6] }
+  },
+  'Food Court 2':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'63343033',
+    'Stalls':{
+      'Stall 201': [5, 'Vietnamese', 7],
+      'Stall 202': [5, 'Western', 6], 
+      'Stall 203': [1, 'Japanese', 6], 
+      'Stall 204': [4, 'Halal', 7], 
+      'Stall 205': [3, 'Vegetarian', 3], 
+      'Stall 206': [5, 'Vietnamese', 7], 
+      'Stall 207': [4, 'Western', 3], 
+      'Stall 208': [2, 'Western', 4], 
+      'Stall 209': [2, 'Western', 3], 
+      'Stall 210': [5, 'Vietnamese', 5] }
+  },
+  'Food Court 9':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'96923456',
+    'Stalls':{
+      'Stall 901': [1, 'Vietnamese', 5], 
+      'Stall 902': [4, 'Indian', 4], 
+      'Stall 903': [4, 'Chinese', 6], 
+      'Stall 904': [2, 'Indian', 3], 
+      'Stall 905': [5, 'Halal', 6], 
+      'Stall 906': [4, 'Japanese', 5], 
+      'Stall 907': [5, 'Halal', 5], 
+      'Stall 908': [2, 'Japanese', 3], 
+      'Stall 909': [5, 'Chinese', 5]}
+  },
+  'Food Court 11':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'97866726',
+    'Stalls':{
+      'Stall 1101': [1, 'Vegetarian', 5], 
+      'Stall 1102': [1, 'Indian', 5], 
+      'Stall 1103': [2, 'Indian', 7], 
+      'Stall 1104': [1, 'Vietnamese', 4], 
+      'Stall 1105': [2, 'Indian', 7], 
+      'Stall 1106': [2, 'Vegetarian', 3]}
+  },  
+  'Food Court 13':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'98510908',
+    'Stalls':{
+      'Stall 1301': [1, 'Halal', 7], 
+      'Stall 1302': [5, 'Korean', 5], 
+      'Stall 1303': [5, 'Western', 5], 
+      'Stall 1304': [2, 'Vietnamese', 3], 
+      'Stall 1305': [5, 'Chinese', 3], 
+      'Stall 1306': [3, 'Korean', 6], 
+      'Stall 1307': [2, 'Japanese', 6], 
+      'Stall 1308': [2, 'Vegetarian', 4]}
+  },
+  'Food Court 14':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'81127239',
+    'Stalls':{
+      'Stall 1401': [3, 'Korean', 3], 
+      'Stall 1402': [4, 'Indian Vegetarian', 6],
+      'Stall 1403': [2, 'Japanese', 5], 
+      'Stall 1404': [5, 'Western', 6], 
+      'Stall 1405': [2, 'Vegetarian', 7], 
+      'Stall 1406': [2, 'Western', 3]}
+  },
+  'Food Court 16':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'94505893',
+    'Stalls':{
+      'Stall 1601': [3, 'Indian', 7], 
+      'Stall 1602': [4, 'Chinese', 7], 
+      'Stall 1603': [5, 'Halal', 4], 
+      'Stall 1604': [1, 'Vegetarian', 6], 
+      'Stall 1605': [3, 'Korean', 5]}
+  },
+  'Foodgle Food Court':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'82963633',
+    'Stalls':{
+      'Foodgle 1': [2, 'Korean', 3], 
+      'Foodgle 2': [1, 'Vietnamese', 7], 
+      'Foodgle 3': [5, 'Vietnamese', 6], 
+      'Foodgle 4': [5, 'Western', 6], 
+      'Foodgle 5': [3, 'Indian', 4], 
+      'Foodgle 6': [4, 'Chinese', 5], 
+      'Foodgle 7': [4, 'Chinese', 7], 
+      'Foodgle 8': [1, 'Western', 7], 
+      'Foodgle 9': [1, 'Indian Vegetarian', 5]}
+  },
+  'North Hill Food Court':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'85080232',
+    'Stalls':{
+      'NH 1': [5, 'Indian', 5], 
+      'NH 2': [1, 'Korean', 5], 
+      'NH 3': [2, 'Japanese', 7], 
+      'NH 4': [4, 'Vegetarian', 6], 
+      'NH 5': [2, 'Vegetarian', 7], 
+      'NH 6': [4, 'Vegetarian', 6], 
+      'NH 7': [5, 'Chinese', 6], 
+      'NH 8': [5, 'Chinese', 5]}
+  },
+  'Pioneer Food Court':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'NA',
+    'Stalls':{
+      'Pioneer 1': [5, 'Indian Vegetarian', 6], 
+      'Pioneer 2': [4, 'Western', 4], 
+      'Pioneer 3': [2, 'Korean', 3], 
+      'Pioneer 4': [3, 'Indian Vegetarian', 4], 
+      'Pioneer 5': [2, 'Western', 6], 
+      'Pioneer 6': [4, 'Halal', 7], 
+      'Pioneer 7': [4, 'Vietnamese', 7], 
+      'Pioneer 8': [3, 'Indian Vegetarian', 3], 
+      'Pioneer 9': [3, 'Vegetarian', 7], 
+      'Pioneer 10': [4, 'Halal', 4], 
+      'Pioneer 11': [3, 'Chinese', 6], 
+      'Pioneer 12': [1, 'Vietnamese', 4]}
+  },  
+}
