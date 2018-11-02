88a82a3dc3225d82698d7d4bdc4fce33358ceda8
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2018-11-02 16:15:43 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,12 +1,18 @@
 # please read the comments
 # if you find sth that weird, you can fix it with a double #, like this:
-# this is flase -> 
+# this is flase ->
 ## this is false
 import pygame, sys
 from pygame.locals import *
 
 pygame.init()
 
+# color
+WHITE   = (255, 255, 255)
+BLACK   = (  0,   0,   0)
+RED     = (255,   0,   0)
+GREEN   = (  0, 255,   0)
+BLUE    = (  0,   0, 255)
 
 
 # stage 1: hello user
@@ -38,34 +44,10 @@ def main():
     screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
     pygame.display.set_caption('NTU F&B')
     stage = 1
-    
+
+
     while True:
         mouseClicked = False
-        if stage == 1:
-            # box 1
-            font1 = pygame.font.SysFont("Arial", 50)
-            text1 = font1.render("Welcome to NTU F&B", True, (255,255,255))
-            textbox1 = text1.get_rect()
-            textbox1.center = (width//2, height//3)
-            screen.blit(text1, textbox1)
-            # box 2
-            font2 = pygame.font.SysFont("Arial", 60)
-            text2 = font2.render("GET STARTED", True, (255, 200, 0), (30, 50, 140))
-            textbox2 = text2.get_rect()
-            textbox2.center = (width//2, height//1.5)
-            screen.blit(text2, textbox2)
-            if textbox2.collidepoint(mouse) and mouseClicked:
-                stage += 1
-        if stage == 2:
-            map = pygame.image.load('ntumap(2).png')
-            map = pygame.transform.scale(map, (width, height))
-            screen.blit(map, (0,0))
-            
-            ## a small button to back to stage 1
-            button1 = pygame.Rect(width-30, 0, 30, 20)
-            pygame.draw.rect(screen, RED, button1)
-            if button1.collidepoint(mouse) and mouseClicked:
-                stage -= 1
 
         for event in pygame.event.get():
             mouse = pygame.mouse.get_pos()
@@ -79,12 +61,38 @@ def main():
             if event.type == pygame.VIDEORESIZE:
                 screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
                 width, height = event.w, event.h
+
+        if stage == 1:
+            # box 1
+            screen.fill(WHITE)
+            font1 = pygame.font.SysFont("Arial", 50)
+            text1 = font1.render("Welcome to NTU F&B", True, BLACK)
+            textbox1 = text1.get_rect()
+            textbox1.center = (width//2, height//3)
+            screen.blit(text1, textbox1)
+            # box 2
+            font2 = pygame.font.SysFont("Arial", 60)
+            text2 = font2.render("GET STARTED", True, (255, 200, 0), (30, 50, 140))
+            textbox2 = text2.get_rect()
+            textbox2.center = (width//2, height//1.5)
+            screen.blit(text2, textbox2)
+
+            if textbox2.collidepoint(mouse) and mouseClicked:
+                stage += 1
+        if stage == 2:
+            map = pygame.image.load('ntumap(2).png')
+            map = pygame.transform.scale(map, (width, height))
+            screen.blit(map, (0,0))
+
+            # a small button to back to stage 1
+            button1 = pygame.Rect(width-30, 0, 30, 20)
+            pygame.draw.rect(screen, RED, button1)
+            if button1.collidepoint(mouse) and mouseClicked:
+                stage -= 1
+
+
         pygame.display.update()
         clock.tick(FPS)
-
-def start(): # hello user
-    start_font1 = pygame.font.SysFont('Helvetica', 60)
-    box1 = writeText(screen, "Welcome to NTU F&B", (255,255,255), None, start_font1, width//2, height//3)
 
 # In case there are so many boxes with text in our interface, this function would work well
 # def writeText(screen, text, text_color, background, font, x_center, y_center):
