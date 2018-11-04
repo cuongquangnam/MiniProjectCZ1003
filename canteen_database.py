1269ece871fc59af4e119fbad5a7792c01a34390
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2018-11-04 22:08:42 +0800

    Added koufu @ south spine, cakes and drinks food type

--- canteen_database.py
+++ canteen_database.py
@@ -5,31 +5,31 @@ canteen_db={
     'Operating hours':'7am to 9pm',
     'Telephone number':'63343033',
     'Stalls':{
-      'Yong Tau Foo':[4,'Chinese',5],
-      'Economical Rice':[5,'Chinese',6],
-      'B & A Chicken Rice-Noodle-Porridge':[5,'Chinese',6],
-      'Teck Seng Restaurant':[4, 'Chinese', 4],
-      'Menya Takashi':[2, 'Japanese', 7],
-      'Hong Jin Yuan':[2, 'Chinese', 5],
-      'Oyster Veg':[1, 'Vegetarian', 3],
-      'La Kopi':[3, 'Breakfast', 4],
-      'Stall 102':[4, 'Vietnamese', 6] }
+      'Stall 101':[4,'Halal',5],
+      'Stall 102':[5,'Japanese',6],
+      'Stall 103':[5,'Japanese',6],
+      'Stall 104':[4, 'Vietnamese', 4],
+      'Stall 105':[2, 'Indian Vegetarian', 7],
+      'Stall 106':[2, 'Korean', 5],
+      'Stall 107':[1, 'Japanese', 3],
+      'Can 1 Cakes':[3, 'Cake', 4],
+      'Can 1 Drinks':[4, 'Drinks', 2] }
   },
   'Food Court 2':{
     #'Location':(xx,yy),
     'Operating hours':'7am to 9pm',
     'Telephone number':'63343033',
     'Stalls':{
-      'Kath\'s Bakery': [5, 'Cake', 7],
-      'Sihuan Cuisine': [5, 'Chinese', 6], 
-      'Shanlong Big Bun': [1, 'Chinese', 6], 
-      'Korean Cuisine': [4, 'Korean', 7], 
-      'Ayam Penyet': [3, 'Indonesian', 3], 
-      'Economical Rice': [5, 'Chinese', 7], 
-      'La Kopi': [4, 'Breakfast', 3], 
-      'Japanese - Western': [2, ['Western'], 4], 
-      'Chicken Rice': [2, 'Chinese', 3], 
-      'Stall 210': [5, 'Vietnamese', 5] }
+      'Stall 201': [5, 'Vietnamese', 7],
+      'Stall 202': [5, 'Western', 6], 
+      'Stall 203': [1, 'Japanese', 6], 
+      'Stall 204': [4, 'Halal', 7], 
+      'Stall 205': [3, 'Vegetarian', 3], 
+      'Stall 206': [5, 'Vietnamese', 7], 
+      'Stall 207': [4, 'Western', 3], 
+      'Stall 208': [2, 'Western', 4], 
+      'Can 2 Cakes': [2, 'Cake', 3], 
+      'Can 2 Drinks': [5, 'Drinks', 2] }
   },
   'Food Court 9':{
     #'Location':(xx,yy),
@@ -44,7 +44,7 @@ canteen_db={
       'Stall 906': [4, 'Japanese', 5], 
       'Stall 907': [5, 'Halal', 5], 
       'Stall 908': [2, 'Japanese', 3], 
-      'Stall 909': [5, 'Chinese', 5]}
+      'Can 9 Drinks': [5, 'Drinks', 2]}
   },
   'Food Court 11':{
     #'Location':(xx,yy),
@@ -56,7 +56,7 @@ canteen_db={
       'Stall 1103': [2, 'Indian', 7], 
       'Stall 1104': [1, 'Vietnamese', 4], 
       'Stall 1105': [2, 'Indian', 7], 
-      'Stall 1106': [2, 'Vegetarian', 3]}
+      'Can 11 Drinks': [2, 'Drinks', 1]}
   },  
   'Food Court 13':{
     #'Location':(xx,yy),
@@ -70,7 +70,7 @@ canteen_db={
       'Stall 1305': [5, 'Chinese', 3], 
       'Stall 1306': [3, 'Korean', 6], 
       'Stall 1307': [2, 'Japanese', 6], 
-      'Stall 1308': [2, 'Vegetarian', 4]}
+      'Can 13 Drinks': [2, 'Drinks', 2]}
   },
   'Food Court 14':{
     #'Location':(xx,yy),
@@ -82,7 +82,7 @@ canteen_db={
       'Stall 1403': [2, 'Japanese', 5], 
       'Stall 1404': [5, 'Western', 6], 
       'Stall 1405': [2, 'Vegetarian', 7], 
-      'Stall 1406': [2, 'Western', 3]}
+      'Can 14 Drinks': [2, 'Drinks', 2]}
   },
   'Food Court 16':{
     #'Location':(xx,yy),
@@ -93,15 +93,15 @@ canteen_db={
       'Stall 1602': [4, 'Chinese', 7], 
       'Stall 1603': [5, 'Halal', 4], 
       'Stall 1604': [1, 'Vegetarian', 6], 
-      'Stall 1605': [3, 'Korean', 5]}
+      'Can 16 Drinks': [3, 'Drinks', 2]}
   },
   'Foodgle Food Court':{
     #'Location':(xx,yy),
     'Operating hours':'7am to 9pm',
     'Telephone number':'82963633',
     'Stalls':{
-      'Foodgle 1': [2, 'Korean', 3], 
-      'Foodgle 2': [1, 'Vietnamese', 7], 
+      'Foodgle Drinks': [2, 'Drinks', 2], 
+      'Foodgle Cake': [1, 'Cake', 3], 
       'Foodgle 3': [5, 'Vietnamese', 6], 
       'Foodgle 4': [5, 'Western', 6], 
       'Foodgle 5': [3, 'Indian', 4], 
@@ -115,8 +115,8 @@ canteen_db={
     'Operating hours':'7am to 9pm',
     'Telephone number':'85080232',
     'Stalls':{
-      'NH 1': [5, 'Indian', 5], 
-      'NH 2': [1, 'Korean', 5], 
+      'NH Drinks': [5, 'Drinks', 1], 
+      'NH Cake': [1, 'Cake', 2], 
       'NH 3': [2, 'Japanese', 7], 
       'NH 4': [4, 'Vegetarian', 6], 
       'NH 5': [2, 'Vegetarian', 7], 
@@ -129,8 +129,8 @@ canteen_db={
     'Operating hours':'7am to 9pm',
     'Telephone number':'NA',
     'Stalls':{
-      'Pioneer 1': [5, 'Indian Vegetarian', 6], 
-      'Pioneer 2': [4, 'Western', 4], 
+      'Pioneer Drinks': [5, 'Drinks', 1], 
+      'Pioneer Cake': [4, 'Cake', 2], 
       'Pioneer 3': [2, 'Korean', 3], 
       'Pioneer 4': [3, 'Indian Vegetarian', 4], 
       'Pioneer 5': [2, 'Western', 6], 
@@ -147,26 +147,45 @@ canteen_db={
     'Operating hours':'7am to 9pm',
     'Telephone number':'NA',
     'Stalls':{
-      "McDonald's": [5, 'Fast Food', 6], 
+      r"McDonald's": [5, 'Fast Food', 6], 
       'Pizza Hut Express': [4, 'Western', 8], 
       'Subway': [3, 'Fast Food', 5], 
       'The Sandwich Guys': [3, 'Fast Food', 4], 
       'The Soup Spoon Union': [4, 'Western', 6], 
       'KFC': [4, 'Fast Food', 7], 
       r"Long John Silver's": [4, 'Fast Food', 7], 
-      'Bakery Cuisine': [3, 'Fast Food', 3], 
+      'Bakery Cuisine': [3, 'Cake', 3], 
       'Mr Bean': [3, 'Fast Food', 3]} 
   },  
-  'Koufu @ the South Spine':{
-    #'Location': (xx,yy),
-    'Operating hours': '7am to 9pm',
-    'Telephone number': 'NA',
+  'Koufu @ South Spine':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'NA',
     'Stalls':{
-  }
-  
+      'Koufu Drinks': [5, 'Drinks', 2], 
+      'Koufu Cake': [1, 'Cake', 3], 
+      'Koufu 3': [2, 'Indian', 7], 
+      'Koufu 4': [5, 'Korean', 3], 
+      'Koufu 5': [5, 'Halal', 4], 
+      'Koufu 6': [4, 'Indian Vegetarian', 7], 
+      'Koufu 7': [1, 'Japanese', 5], 
+      'Koufu 8': [1, 'Halal', 7], 
+      'Koufu 9': [1, 'Vegetarian', 5], 
+      'Koufu 10': [3, 'Chinese', 6], 
+      'Koufu 11': [1, 'Korean', 5], 
+      'Koufu 12': [4, 'Japanese', 5], 
+      'Koufu 13': [5, 'Western', 6], 
+      'Koufu 14': [1, 'Vietnamese', 6], 
+      'Koufu 15': [1, 'Chinese', 6]}
+  }, 
 }
 
 # list of all canteens and stalls
-filtered_list=[('Food Court 1', 'Stall 101'), ('Food Court 1', 'Stall 102'), ('Food Court 1', 'Stall 103'), ('Food Court 1', 'Stall 104'), ('Food Court 1', 'Stall 105'), ('Food Court 1', 'Stall 106'), ('Food Court 1', 'Stall 107'), ('Food Court 1', 'Stall 108'), ('Food Court 1', 'Stall 109'), ('Food Court 2', 'Stall 201'), ('Food Court 2', 'Stall 202'), ('Food Court 2', 'Stall 203'), ('Food Court 2', 'Stall 204'), ('Food Court 2', 'Stall 205'), ('Food Court 2', 'Stall 206'), ('Food Court 2', 'Stall 207'), ('Food Court 2', 'Stall 208'), ('Food Court 2', 'Stall 209'), ('Food Court 2', 'Stall 210'), ('Food Court 9', 'Stall 901'), ('Food Court 9', 'Stall 902'), ('Food Court 9', 'Stall 903'), ('Food Court 9', 'Stall 904'), ('Food Court 9', 'Stall 905'), ('Food Court 9', 'Stall 906'), ('Food Court 9', 'Stall 907'), ('Food Court 9', 'Stall 908'), ('Food Court 9', 'Stall 909'), ('Food Court 11', 'Stall 1101'), ('Food Court 11', 'Stall 1102'), ('Food Court 11', 'Stall 1103'), ('Food Court 11', 'Stall 1104'), ('Food Court 11', 'Stall 1105'), ('Food Court 11', 'Stall 1106'), ('Food Court 13', 'Stall 1301'), ('Food Court 13', 'Stall 1302'), ('Food Court 13', 'Stall 1303'), ('Food Court 13', 'Stall 1304'), ('Food Court 13', 'Stall 1305'), ('Food Court 13', 'Stall 1306'), ('Food Court 13', 'Stall 1307'), ('Food Court 13', 'Stall 1308'), ('Food Court 14', 'Stall 1401'), ('Food Court 14', 'Stall 1402'), ('Food Court 14', 'Stall 1403'), ('Food Court 14', 'Stall 1404'), ('Food Court 14', 'Stall 1405'), ('Food Court 14', 'Stall 1406'), ('Food Court 16', 'Stall 1601'), ('Food Court 16', 'Stall 1602'), ('Food Court 16', 'Stall 1603'), ('Food Court 16', 'Stall 1604'), ('Food Court 16', 'Stall 1605'), ('Foodgle Food Court', 'Foodgle 1'), ('Foodgle Food Court', 'Foodgle 2'), ('Foodgle Food Court', 'Foodgle 3'), ('Foodgle Food Court', 'Foodgle 4'), ('Foodgle Food Court', 'Foodgle 5'), ('Foodgle Food Court', 'Foodgle 6'), ('Foodgle Food Court', 'Foodgle 7'), ('Foodgle Food Court', 'Foodgle 8'), ('Foodgle Food Court', 'Foodgle 9'), ('North Hill Food Court', 'NH 1'), ('North Hill Food Court', 'NH 2'), ('North Hill Food Court', 'NH 3'), ('North Hill Food Court', 'NH 4'), ('North Hill Food Court', 'NH 5'), ('North Hill Food Court', 'NH 6'), ('North Hill Food Court', 'NH 7'), ('North Hill Food Court', 'NH 8'), ('Pioneer Food Court', 'Pioneer 1'), ('Pioneer Food Court', 'Pioneer 2'), ('Pioneer Food Court', 'Pioneer 3'), ('Pioneer Food Court', 'Pioneer 4'), ('Pioneer Food Court', 'Pioneer 5'), ('Pioneer Food Court', 'Pioneer 6'), ('Pioneer Food Court', 'Pioneer 7'), ('Pioneer Food Court', 'Pioneer 8'), ('Pioneer Food Court', 'Pioneer 9'), ('Pioneer Food Court', 'Pioneer 10'), ('Pioneer Food Court', 'Pioneer 11'), ('Pioneer Food Court', 'Pioneer 12'), ('North Spine Plaza', "McDonald's"), ('North Spine Plaza', 'Pizza Hut Express'), ('North Spine Plaza', 'Subway'), ('North Spine Plaza', 'The Sandwich Guys'), ('North Spine Plaza', 'The Soup Spoon Union'), ('North Spine Plaza', 'KFC'), ('North Spine Plaza', "Long John Silver's"), ('North Spine Plaza', 'Bakery Cuisine'), ('North Spine Plaza', 'Mr Bean')]
+filtered_list=[]
+for canteen in canteen_db.keys():
+  for stall in canteen_db[canteen]['Stalls'].keys():
+    filtered_list.append((canteen,stall))
+
 # various options of food types
-foodtype=('Halal','Vegetarian','Indian','Vietnamese','Western','Chinese','Indian Vegetarian','Japanese','Korean','Fast Food')
+foodtype=('Halal','Vegetarian','Indian','Vietnamese','Western','Chinese','Indian Vegetarian','Japanese','Korean','Fast Food', 'Drinks', 'Cake')
+
