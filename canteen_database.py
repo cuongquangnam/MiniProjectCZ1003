a7a8d008d9a620652a2732b7ccb54772754b6511
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2018-11-04 16:49:55 +0800

    Included North Spine Plaza stalls

--- canteen_database.py
+++ canteen_database.py
@@ -141,5 +141,25 @@ canteen_db={
       'Pioneer 10': [4, 'Halal', 4], 
       'Pioneer 11': [3, 'Chinese', 6], 
       'Pioneer 12': [1, 'Vietnamese', 4]}
+  }, 
+  'North Spine Plaza':{
+    #'Location':(xx,yy),
+    'Operating hours':'7am to 9pm',
+    'Telephone number':'NA',
+    'Stalls':{
+      r"McDonald's": [5, 'Fast Food', 6], 
+      'Pizza Hut Express': [4, 'Western', 8], 
+      'Subway': [3, 'Fast Food', 5], 
+      'The Sandwich Guys': [3, 'Fast Food', 4], 
+      'The Soup Spoon Union': [4, 'Western', 6], 
+      'KFC': [4, 'Fast Food', 7], 
+      r"Long John Silver's": [4, 'Fast Food', 7], 
+      'Bakery Cuisine': [3, 'Fast Food', 3], 
+      'Mr Bean': [3, 'Fast Food', 3]} 
   },  
 }
+
+# list of all canteens and stalls
+filtered_list=[('Food Court 1', 'Stall 101'), ('Food Court 1', 'Stall 102'), ('Food Court 1', 'Stall 103'), ('Food Court 1', 'Stall 104'), ('Food Court 1', 'Stall 105'), ('Food Court 1', 'Stall 106'), ('Food Court 1', 'Stall 107'), ('Food Court 1', 'Stall 108'), ('Food Court 1', 'Stall 109'), ('Food Court 2', 'Stall 201'), ('Food Court 2', 'Stall 202'), ('Food Court 2', 'Stall 203'), ('Food Court 2', 'Stall 204'), ('Food Court 2', 'Stall 205'), ('Food Court 2', 'Stall 206'), ('Food Court 2', 'Stall 207'), ('Food Court 2', 'Stall 208'), ('Food Court 2', 'Stall 209'), ('Food Court 2', 'Stall 210'), ('Food Court 9', 'Stall 901'), ('Food Court 9', 'Stall 902'), ('Food Court 9', 'Stall 903'), ('Food Court 9', 'Stall 904'), ('Food Court 9', 'Stall 905'), ('Food Court 9', 'Stall 906'), ('Food Court 9', 'Stall 907'), ('Food Court 9', 'Stall 908'), ('Food Court 9', 'Stall 909'), ('Food Court 11', 'Stall 1101'), ('Food Court 11', 'Stall 1102'), ('Food Court 11', 'Stall 1103'), ('Food Court 11', 'Stall 1104'), ('Food Court 11', 'Stall 1105'), ('Food Court 11', 'Stall 1106'), ('Food Court 13', 'Stall 1301'), ('Food Court 13', 'Stall 1302'), ('Food Court 13', 'Stall 1303'), ('Food Court 13', 'Stall 1304'), ('Food Court 13', 'Stall 1305'), ('Food Court 13', 'Stall 1306'), ('Food Court 13', 'Stall 1307'), ('Food Court 13', 'Stall 1308'), ('Food Court 14', 'Stall 1401'), ('Food Court 14', 'Stall 1402'), ('Food Court 14', 'Stall 1403'), ('Food Court 14', 'Stall 1404'), ('Food Court 14', 'Stall 1405'), ('Food Court 14', 'Stall 1406'), ('Food Court 16', 'Stall 1601'), ('Food Court 16', 'Stall 1602'), ('Food Court 16', 'Stall 1603'), ('Food Court 16', 'Stall 1604'), ('Food Court 16', 'Stall 1605'), ('Foodgle Food Court', 'Foodgle 1'), ('Foodgle Food Court', 'Foodgle 2'), ('Foodgle Food Court', 'Foodgle 3'), ('Foodgle Food Court', 'Foodgle 4'), ('Foodgle Food Court', 'Foodgle 5'), ('Foodgle Food Court', 'Foodgle 6'), ('Foodgle Food Court', 'Foodgle 7'), ('Foodgle Food Court', 'Foodgle 8'), ('Foodgle Food Court', 'Foodgle 9'), ('North Hill Food Court', 'NH 1'), ('North Hill Food Court', 'NH 2'), ('North Hill Food Court', 'NH 3'), ('North Hill Food Court', 'NH 4'), ('North Hill Food Court', 'NH 5'), ('North Hill Food Court', 'NH 6'), ('North Hill Food Court', 'NH 7'), ('North Hill Food Court', 'NH 8'), ('Pioneer Food Court', 'Pioneer 1'), ('Pioneer Food Court', 'Pioneer 2'), ('Pioneer Food Court', 'Pioneer 3'), ('Pioneer Food Court', 'Pioneer 4'), ('Pioneer Food Court', 'Pioneer 5'), ('Pioneer Food Court', 'Pioneer 6'), ('Pioneer Food Court', 'Pioneer 7'), ('Pioneer Food Court', 'Pioneer 8'), ('Pioneer Food Court', 'Pioneer 9'), ('Pioneer Food Court', 'Pioneer 10'), ('Pioneer Food Court', 'Pioneer 11'), ('Pioneer Food Court', 'Pioneer 12'), ('North Spine Plaza', "McDonald's"), ('North Spine Plaza', 'Pizza Hut Express'), ('North Spine Plaza', 'Subway'), ('North Spine Plaza', 'The Sandwich Guys'), ('North Spine Plaza', 'The Soup Spoon Union'), ('North Spine Plaza', 'KFC'), ('North Spine Plaza', "Long John Silver's"), ('North Spine Plaza', 'Bakery Cuisine'), ('North Spine Plaza', 'Mr Bean')]
+# various options of food types
+foodtype=('Halal','Vegetarian','Indian','Vietnamese','Western','Chinese','Indian Vegetarian','Japanese','Korean','Fast Food')
