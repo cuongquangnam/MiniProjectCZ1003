d0c55f9a220b08a487cd385778359cc93e9a228e
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2018-11-15 00:03:51 +0800

    Add files via upload

---
+++ abc/function.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+import openpyxl
+
+df = pd.read_excel('canteen_db.xlsx')
+infocan = pd.read_excel('canteen details.xlsx')
+admin_data = pd.read_excel("Admin.xlsx")
+wb = openpyxl.load_workbook('Canteen_db - Copy.xlsx')
+ws = wb['Sheet1']
+
+infocan = infocan.set_index('Canteen')
+df = df.set_index(['Canteen'])
+#takes in foodtype = ['Food1','food2' etc], pricerange = [lower, higher as floats/int], the search term
+# rating = int(1 to 5) or 0 if not specified
+
+def search(ws, lst):
+    rows = []
+    for row_num in range (2, ws.max_row+1):
+        can = ws.cell(row = row_num, column = 1).value
+        stall = ws.cell(row = row_num, column = 3).value
+        food = ws.cell(row = row_num, column = 4).value
+        #if lst[2] == 'n' then we just need to assign a value to food
+        #suc that food == lst[2]
+        if lst[2] == 'n':
+            food = 'n'
+        if can == lst[0] and stall == lst[1] and food == lst[2]:
+            rows.append(row_num)
+    return rows
+
+def searchfood(foodtype, pricerange, rating, search, df):
+    #copy the dataframe to temporary.
+    search_df = df.copy()
+    #filter by foodtype
+    if foodtype != []:
+        foodcond = search_df['Food Type'].isin(foodtype)
+        search_df = search_df[foodcond]
+    # filter by price range
+    low_price = pricerange[0]
+    high_price = pricerange[1]
+    pricecond1 = search_df['Price'] >= low_price
+    pricecond2 = search_df['Price'] <= high_price
+    search_df = search_df[ pricecond1 & pricecond2 ]
+    # filter by rating, shows all above specified rating
+    if rating != 0:
+        ratingcond = search_df['Rating'] >= rating
+        search_df = search_df[ ratingcond ]
+    # filter by menu Item
+    if search != ' ':
+        lst = search.lower().split()
+        for word in lst:
+            wordcond = search_df['Menu Item'].str.lower().str.contains(word)
+            search_df = search_df[wordcond]
+    # return the filtered DataFrame
+    return search_df
+#function to sort by rating given the DataFrame filtered, the output is a list of indexes
+def sort_by_rating(filter_df):
+    return filter_df.sort_values("Rating")
+#function to sort by price given the DataFrame filtered, the output is a list of indexes
+def sort_by_price(filter_df):
+    return filter_df.sort_values("Price")
+#function to sort by distance based on the user location, the filtered DataFrame and the DataFrame containing
+#information about the canteen
+def sort_by_location(user_loc, filter_df, infocan):
+    lst_loc = filter_df.index.unique()
+    lst_dist = {}
+    frames = []
+    for loc in lst_loc:
+        #calculate the distance
+        distance = ((user_loc[0] - infocan.loc[loc]['loc x'])**2 + (user_loc[1] - infocan.loc[loc]['loc y'])**2)**(1/2)
+        #convert from bitmap to km (just giving an approximate of the distance)
+        distance *= 0.0025
+        #create a new column in DataFrame to store all of the distances
+        filter_df.at[loc,'Distance'] = distance
+    #sort DataFrame according to the distance
+    filter_df = filter_df.sort_values('Distance')
+    # select top 10 location to show the user
+    count = 1
+    for loc in filter_df.index.unique():
+        a = filter_df.loc[loc]
+        if len(a.shape) == 1:
+            b = pd.DataFrame(a).T
+            frames.append(b)
+        else:
+            frames.append(a)
+        if count == 10:
+            break
+        count += 1
+    if frames !=[]:
+        #concatenate all the DataFrames selected
+        return pd.concat(frames)
+    else:
+        return pd.DataFrame()
+
+def display10(filter_df):
+    canteen_list_10 = filter_df.index.unique()[:10]
+    return filter_df.loc[canteen_list_10,:]
+
+def get_location(filter_df):
+    df = filter_df
+    if type(filter_df) == np.float64:
+        return [df]
+    else: return df
+
+result = searchfood(['Western'], [5,5], 1, 'spaghetti', df)
+t = sort_by_location((441,430), result, infocan)
+t1 = result.loc["NIE"]
+# print(type(t1[3]))
+
+
+result = searchfood([],[0.0,100.0],0,'burger', df)
+# t = sort_by_location((333,222), result, infocan)
+t = sort_by_price(result)
+u = sort_by_rating(result)
+# print(result.loc["Canteen 9", "Stall"])
+# print(result)
+canteen = "Canteen 2"
+t1 = infocan.loc[canteen, ["Open", "Closed"]]
---
+++ abc/update.py
@@ -0,0 +1,93 @@
+import openpyxl
+import pandas as pd
+from pandas import DataFrame
+#load the worksheet from the excel file
+wb = openpyxl.load_workbook('Canteen_db - Copy.xlsx')
+ws = wb['Sheet1']
+#this is for the admin to login into their account
+def login():
+    df = pd.read_excel('Admin.xlsx')
+    check = False
+    while (check == False):
+        admin = input('Admin??')
+        password = input('Password??')
+        condadmin = df['Admin account'] == admin
+        condpass = df['Password'] == password
+        result = df[condadmin & condpass]
+        if result.empty:
+            print('Invalide username or password')
+        else:
+            check = True
+#this is a fucntion to ask the user basic information
+def ask():
+    canteen = input("Canteen??")
+    stall = input('Stall??')
+    #food == 'n' means that the user don't want to input the food name,
+    #they just want to deal with the canteen and stall
+    food = input('Food??')
+    return (canteen, stall, food)
+#function to search given list (canteen, stall, food), output the list of
+#rows containing the information
+def search(ws, lst):
+    rows = []
+    for row_num in range (2, ws.max_row+1):
+        can = ws.cell(row = row_num, column = 1).value
+        stall = ws.cell(row = row_num, column = 3).value
+        food = ws.cell(row = row_num, column = 4).value
+        #if lst[2] == 'n' then we just need to assign a value to food
+        #suc that food == lst[2]
+        if lst[2] == 'n':
+            food = 'n'
+        if can == lst[0] and stall == lst[1] and food == lst[2]:
+            rows.append(row_num)
+    return rows
+#function to edit the worksheet
+def edit(ws):
+    info = ask()
+    result = search(ws, info)
+    if result == []:
+        print('Oops! We can\'t find the food you inquire')
+    else:
+        #info[2] == 'n' means that they want to edit the rating of the stall
+        #else they want to edit the price of the food
+        if info[2] == 'n':
+            rating = int(input('Rating'))
+            x = input('Are you sure? (y/n)')
+            if x == 'y':
+                for row_num in result:
+                    ws.cell(row = row_num, column = 6, value = rating)
+        else:
+            price = int(input('Price'))
+            x = input('Are you sure? (y/n)')
+            if x == 'y':
+                for row_num in result:
+                    ws.cell(row = row_num, column = 5, value = price)
+#function to remove some information in the worksheet
+def remove(ws):
+    info = ask()
+    result = search(ws, info)
+    x = input('Are you sure? (y/n)')
+    if x == 'y':
+        if result == []:
+            print('Oops! We can\'t find the food you inquire')
+        else:
+            result.reverse()
+            for row_num in result:
+                ws.delete_rows(row_num)
+#function to add information into the worksheet
+def add(ws):
+    info = ask()
+    result = search(ws, info)
+    x = input('Are you sure? (y/n)')
+    if x == 'y':
+        if result != []:
+            print("Sorry! The database have already had this information")
+        else:
+            price = input('Price??')
+            rating = input('Rating??')
+            foodtype = input('Type of food??')
+            ws.append([info[0],foodtype, info[1], info[2], price, rating])
+#remove(ws)
+#remember to save the workbook after updating the information
+wb.save('Canteen_db - Copy.xlsx')
+login()
