6db40778359a18f3b514ab83d3c19ed9ad417f1e
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2018-11-14 10:10:57 +0800

    Update spellchecker.py

--- spellchecker.py
+++ spellchecker.py
@@ -4,38 +4,44 @@ from collections import Counter
 class SpellChecker:
     def __init__(self):
         def words(text): return re.findall(r'\w+', text.lower())
-        self.WORDS = Counter(words(open('menu_items.txt').read())) 
-# counter returns a dictionary, keys are the words 
-# that appear and values are the times it appear
+        self.WORDS = Counter(words(open('menu_items.txt').read()))  
+        #WORDS is a dictionary of all the words in menu_items.txt, which is all the words from menu items in canteen db
+         #  keys are the words that appear and values are the times it appear
 
     def P(self,word, N=sum(WORDS.values())): 
-    # "Probability of `word`." 
+    # "Probability of `word`." appearing in db WORDS
         return WORDS[word] / N
 
     def correction(self,word): 
-    # "Most probable spelling correction for word."
+    # "Most probable spelling correction for word." >> return the word based on the highest probability out of WORDS data set
         return max(candidates(word), key=P)
 
     def candidates(self,word): 
-    #"Generate possible spelling corrections for word."
+    #"Generate possible spelling corrections for word."  #based on the first non empty set in order of priority, 
+    #if word is already in known list of WORDS, or words 1 edit distance away is in known list of words, 2 edits, or just word if it is not known.
         return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
 
     def known(self,words): 
-   # "The subset of `words` that appear in the dictionary of WORDS."
+   # "The subset of `words` that appear in the dictionary of WORDS." 
         return set(w for w in words if w in WORDS)
 
     def edits1(self,word):
-    #"All edits that are one edit away from `word`."
+    #" return set of All edits that are one edit away from `word`."
         letters    = 'abcdefghijklmnopqrstuvwxyz'
-        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-        deletes    = [L + R[1:]               for L, R in splits if R]
-        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-        replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-        inserts    = [L + c + R               for L, R in splits for c in letters]
-        return set(deletes + transposes + replaces + inserts)
+        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]  #split the words for manipulation
+        deletes    = [L + R[1:]               for L, R in splits if R]        #words that have one letter deleted
+        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]   #words that swap position of one letter
+        replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters] #words that replace one letter with another alphabet
+        inserts    = [L + c + R               for L, R in splits for c in letters]  #words that insert one letter in between word
+        return set(deletes + transposes + replaces + inserts) 
 
     def edits2(self,word): 
     #"All edits that are two edits away from `word`."
         return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
     #adapted from http://norvig.com/spell-correct.html 
+
+   #### we only need to use this commands
+#s = SpellChecker()
+# INPUT >> s.correction('chjcken') #command to return most likely spell corrected word 
+# OUTPUT>>>> 'chicken' 
