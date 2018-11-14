c05290f7cfb5bbccff13c55bfff3b4a0d3c8daf8
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2018-11-14 10:34:52 +0800

    Update spellchecker.py

--- spellchecker.py
+++ spellchecker.py
@@ -4,26 +4,26 @@ from collections import Counter
 class SpellChecker:
     def __init__(self):
         def words(text): return re.findall(r'\w+', text.lower())
-        self.WORDS = Counter(words(open('menu_items.txt').read()))  
+        self.WORDS = Counter(words(open('menu_items.txt').read()))
+        self.N=sum(self.WORDS.values())
         #WORDS is a dictionary of all the words in menu_items.txt, which is all the words from menu items in canteen db
          #  keys are the words that appear and values are the times it appear
-
-    def P(self,word, N=sum(WORDS.values())): 
-    # "Probability of `word`." appearing in db WORDS
-        return WORDS[word] / N
+    def P(self,word): 
+    # "Probability of `word`." 
+        return self.WORDS[word] / self.N
 
     def correction(self,word): 
     # "Most probable spelling correction for word." >> return the word based on the highest probability out of WORDS data set
-        return max(candidates(word), key=P)
+        return max(self.candidates(word), key=self.P)
 
     def candidates(self,word): 
     #"Generate possible spelling corrections for word."  #based on the first non empty set in order of priority, 
     #if word is already in known list of WORDS, or words 1 edit distance away is in known list of words, 2 edits, or just word if it is not known.
-        return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+        return (self.known([word]) or self.known(self.edits1(word)) or self.known(self.edits2(word)) or [word])
 
     def known(self,words): 
    # "The subset of `words` that appear in the dictionary of WORDS." 
-        return set(w for w in words if w in WORDS)
+        return set(w for w in words if w in self.WORDS)
 
     def edits1(self,word):
     #" return set of All edits that are one edit away from `word`."
@@ -37,7 +37,7 @@ class SpellChecker:
 
     def edits2(self,word): 
     #"All edits that are two edits away from `word`."
-        return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+        return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))
 
     #adapted from http://norvig.com/spell-correct.html 
 
